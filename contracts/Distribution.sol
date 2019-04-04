27542e74bd92bf2b65bd39879d3216b665e536d9
ggarri <gabriel.garrido.work@gmail.com>
2019-04-04 13:10:10 +0200

    Adjusted token pool sizes

--- contracts/Distribution.sol
+++ contracts/Distribution.sol
@@ -7,23 +7,23 @@ import "./Ownable.sol";
 contract Distribution is Ownable, Vesting {
   using SafeMath for uint256;
 
-  enum Category {TEAM, SEED_CONTRIBUTORS, FOUNDERS, ADVISORS, CONSULTANTS, OTHER}
+  enum Category {TEAM, SEED_CONTRIBUTORS, CONSULTANTS, OTHER, FUTURE_OFFERING}
 
   uint256 private constant decimalFactor = 10 ** uint256(18);
   uint256 public MAX_TOKENS = 300000000 * decimalFactor;
 
   // Todo: SHOULD THIS STILL BE SO SPECIFIC? TO BE DISCUSSED
-  uint256 public PROJECT_INITIAL_SUPPLY = 135000000 * decimalFactor;
+  uint256 public PROJECT_INITIAL_SUPPLY = 135000000 * decimalFactor; // 45.00% of total amount
+  uint256 public AVAILABLE_TEAM_SUPPLY = 60000000 * decimalFactor; // 20.00% released over 24 months
+  uint256 public AVAILABLE_SEED_CONTRIBUTORS_SUPPLY = 24000000 * decimalFactor; // 8.00% released over 5 months
+  uint256 public AVAILABLE_CONSULTANTS_SUPPLY = 2000000 * decimalFactor; //  1.00% released at Token Distribution (TD)
+  uint256 public AVAILABLE_OTHER_SUPPLY = 21000000 * decimalFactor; //  7.00% released at Token Distribution (TD)
+  uint256 public AVAILABLE_FUTURE_OFFERING_SUPPLY = 28000000 * decimalFactor; //  9.00% released at Token Distribution (TD)
   uint256 public PROJECT_AVAILABLE_TOTAL_SUPPLY = PROJECT_INITIAL_SUPPLY;
-  uint256 public AVAILABLE_TEAM_SUPPLY = 65424000 * decimalFactor; // 21.81% released over 24 months
-  uint256 public AVAILABLE_SEED_CONTRIBUTORS_SUPPLY = 36000000 * decimalFactor; // 12.00% released over 5 months
-  uint256 public AVAILABLE_FOUNDERS_SUPPLY = 15000000 * decimalFactor; //  5.00% released over 24 months
-  uint256 public AVAILABLE_ADVISORS_SUPPLY = 122100 * decimalFactor; //  0.04% released at Token Distribution (TD)
-  uint256 public AVAILABLE_CONSULTANTS_SUPPLY = 1891300 * decimalFactor; //  0.63% released at Token Distribution (TD)
-  uint256 public AVAILABLE_OTHER_SUPPLY = 16562600 * decimalFactor; //  5.52% released at Token Distribution (TD)
 
-  uint256 public SALE_INITIAL_SUPPLY = 165000000 * decimalFactor;
+  uint256 public SALE_INITIAL_SUPPLY = 165000000 * decimalFactor; // 55.00% of total amount
   uint256 public SALE_AVAILABLE_TOTAL_SUPPLY = SALE_INITIAL_SUPPLY;
+
   uint256 private constant BONUS_MIN = 0;
   uint256 private constant BONUS_MAX = 40;
 
@@ -36,7 +36,7 @@ contract Distribution is Ownable, Vesting {
     // make sure the start time is in the future
     require(_openingTime >= now);
     // the total of the different pools must equal the total project supply
-    require(PROJECT_AVAILABLE_TOTAL_SUPPLY == AVAILABLE_TEAM_SUPPLY.add(AVAILABLE_SEED_CONTRIBUTORS_SUPPLY).add(AVAILABLE_FOUNDERS_SUPPLY).add(AVAILABLE_ADVISORS_SUPPLY).add(AVAILABLE_CONSULTANTS_SUPPLY).add(AVAILABLE_OTHER_SUPPLY)); 
+    require(PROJECT_AVAILABLE_TOTAL_SUPPLY == AVAILABLE_TEAM_SUPPLY.add(AVAILABLE_SEED_CONTRIBUTORS_SUPPLY).add(AVAILABLE_CONSULTANTS_SUPPLY).add(AVAILABLE_OTHER_SUPPLY).add(AVAILABLE_FUTURE_OFFERING_SUPPLY));
     // all tokens are split between sale and project
     require(MAX_TOKENS == PROJECT_INITIAL_SUPPLY.add(SALE_INITIAL_SUPPLY));
       
@@ -62,17 +62,7 @@ contract Distribution is Ownable, Vesting {
 
       AVAILABLE_SEED_CONTRIBUTORS_SUPPLY = AVAILABLE_SEED_CONTRIBUTORS_SUPPLY.sub(_amount);
       setVestingSchedule(_beneficiary, _amount, 0, now, now + 150 days, 30 days, false);
-    } else if (_category == Category.FOUNDERS) {
-      _validateScheduleProjectVesting(_beneficiary, _amount, AVAILABLE_FOUNDERS_SUPPLY);
-
-      AVAILABLE_FOUNDERS_SUPPLY = AVAILABLE_FOUNDERS_SUPPLY.sub(_amount);
-      setVestingSchedule(_beneficiary, _amount, 0, now, now + 720 days, 30 days, false);
-    } else if (_category == Category.ADVISORS) {
-      _validateScheduleProjectVesting(_beneficiary, _amount, AVAILABLE_ADVISORS_SUPPLY);
-
-      AVAILABLE_ADVISORS_SUPPLY = AVAILABLE_ADVISORS_SUPPLY.sub(_amount);
-      _beneficiary.transfer(_amount);
-    } else if (_category == Category.CONSULTANTS) {
+    }  else if (_category == Category.CONSULTANTS) {
       _validateScheduleProjectVesting(_beneficiary, _amount, AVAILABLE_CONSULTANTS_SUPPLY);
 
       AVAILABLE_CONSULTANTS_SUPPLY = AVAILABLE_CONSULTANTS_SUPPLY.sub(_amount);
@@ -81,6 +71,11 @@ contract Distribution is Ownable, Vesting {
       _validateScheduleProjectVesting(_beneficiary, _amount, AVAILABLE_OTHER_SUPPLY);
 
       AVAILABLE_OTHER_SUPPLY = AVAILABLE_OTHER_SUPPLY.sub(_amount);
+      _beneficiary.transfer(_amount);
+    } else if (_category == Category.FUTURE_OFFERING) {
+      _validateScheduleProjectVesting(_beneficiary, _amount, AVAILABLE_FUTURE_OFFERING_SUPPLY);
+
+      AVAILABLE_FUTURE_OFFERING_SUPPLY = AVAILABLE_FUTURE_OFFERING_SUPPLY.sub(_amount);
       _beneficiary.transfer(_amount);
     } else {
       require(true == false, "project category not supported");
